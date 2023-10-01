8f9c63b007bfbb5575cb56f73150fa1f16ed878c
dlarlwns135 <dlarlwns135@naver.com>
2023-10-01 15:30:03 +0900

    최종 완성
    걍 쉽다

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -17,6 +17,8 @@ def handle_events():
     for event in events:
         if event.type == SDL_QUIT:
             running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
         elif event.type == SDL_MOUSEMOTION:
             mx, my = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_MOUSEBUTTONDOWN:
@@ -30,85 +32,59 @@ x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 hide_cursor()
 click_positions = []  # 클릭한 위치 저장 리스트
 current_target = None
+see_right = True
 
 while running:
     handle_events()
-    clear_canvas()
-    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    hand.draw(mx, my)
-    update_canvas()
-
-    frame = (frame + 1) % 8
     if not running:
         break
+    clear_canvas()
+    TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+    if see_right:
+        character.clip_draw(frame * 100, 100 * 3, 100, 100, x, y)
+    else:
+        character.clip_draw(frame * 100, 100 * 2, 100, 100, x, y)
+    hand.draw(mx, my)
+    for pos in click_positions:
+        hand.draw(pos[0], pos[1])
+    update_canvas()
+    frame = (frame + 1) % 8
+    if click_positions:
+        for pos in click_positions:
+            hand.draw(pos[0],pos[1])
+        if current_target is None:
+            current_target = click_positions[0]
+            dx, dy = current_target
+            sx, sy = x, y
+
+            for i in range(0, 300 + 1, 1):
+                t = (i / 300)
+                x = (1 - t) * x + t * dx
+                y = (1 - t) * y + t * dy
+                clear_canvas()
+                TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+                if sx >= dx:
+                    character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
+                    see_right = False
+                else:
+                    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+                    see_right = True
+                handle_events()
+                if not running:
+                    break
+                hand.draw(mx, my)
+                for pos in click_positions:
+                    hand.draw(pos[0], pos[1])
+                frame = (frame + 1) % 8
+                dis = math.sqrt((dx - x) ** 2 + (dy - y) ** 2)
+                if dis < 1:
+                    current_target = None
+                    click_positions.pop(0)
+                    break
+                update_canvas()
+
+                delay(0.01)
+
     delay(0.01)
 
 close_canvas()
-
-# def handle_events():
-#     global running
-#     global click_positions, mx, my
-#     events = get_events()
-#     for event in events:
-#         if event.type == SDL_QUIT:
-#             running = False
-#         elif event.type == SDL_MOUSEMOTION:
-#             mx, my = event.x, TUK_HEIGHT - 1 - event.y
-#         elif event.type == SDL_MOUSEBUTTONDOWN:
-#             if event.button == SDL_BUTTON_LEFT:
-#                 click_positions.append((event.x, TUK_HEIGHT - 1 - event.y))
-#
-#
-# running = True
-# frame = 0
-# x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-# mx, my, sx, sy = 0, 0, 0, 0
-#
-# click_positions = []  # 클릭한 위치 저장 리스트
-# current_target = None
-#
-# while running:
-#     handle_events()
-#
-#     if current_target is None:
-#         if click_positions:
-#             current_target = click_positions[0]  # 리스트에서 현재 목표 좌표를 가져옴
-#             mx, my = current_target
-#             sx, sy = x, y
-#
-#     for i in range(0, 200 + 1, 1):
-#         t = (i / 200)
-#         x = (1 - t) * sx + t * mx
-#         y = (1 - t) * sy + t * my
-#         clear_canvas()
-#         TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-#
-#         # 클릭한 모든 위치에 화살표 이미지를 그립니다.
-#         for pos in click_positions:
-#             hand.draw(pos[0], pos[1])
-#
-#         # 현재 목표 위치에 화살표 이미지를 그립니다.
-#         if current_target:
-#             hand.draw(mx, my)
-#
-#         if sx >= mx:
-#             character.clip_draw(frame * 100, 100 * 0, 100, 100, x, y)
-#         else:
-#             character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-#         update_canvas()
-#         frame = (frame + 1) % 8
-#
-#         dis = math.sqrt((mx - x) ** 2 + (my - y) ** 2)
-#         if dis < 1:
-#             if current_target:
-#                 x, y = mx, my  # 목표 위치에 도달하면 캐릭터 위치를 목표 위치로 설정
-#                 current_target = None  # 현재 목표 위치를 비움
-#                 if click_positions:
-#                     click_positions.pop(0)  # 도착한 좌표를 리스트에서 삭제
-#
-#     if not running:
-#         break
-#     delay(0.01)
-#
-# close_canvas()
